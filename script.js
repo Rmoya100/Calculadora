d7a571bb49a9dc918cd1eb9da825e70f5d688e7f
Ronnie Moya <rfmoya100@gmail.com>
2026-02-19 18:38:44 -0300

    Archivos Finales

---
+++ script.js
@@ -0,0 +1,40 @@
+const display = document.querySelector("#display");
+const buttons = document.querySelectorAll("button");
+
+buttons.forEach((item) => {
+
+    item.onclick = () => {
+        
+        if (item.id == "clear") {
+
+            display.innerText = "";
+
+        } else if (item.id == "backspace") {
+
+            let string = display.innerText.toString();
+            display.innerText = string.substr(0, string.length - 1);
+
+        } else if (display.innerText != "" && item.id == "equal") {
+
+            display.innerText = eval(display.innerText);
+
+        } else if (display.innerText == "" && item.id == "equal") {
+
+            display.innerText = "NULL";
+            setTimeout(() => (display.innerText = ""), 500);
+
+        }else{
+
+            display.innerText+=item.id
+        } 
+    }
+})
+const themetoggle = document.querySelector(".theme-toggler");
+const calculator = document.querySelector(".calculadora");
+
+let isDark = true;
+themetoggle.onclick=()=>{
+    calculator.classList.toggle("dark")
+    themetoggle.classList.toggle("active")
+    isDark=!isDark
+}
